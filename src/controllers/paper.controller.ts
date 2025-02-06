0ca2005c1f98388dcd506f6eafdf13621fc74c50
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2025-02-06 20:09:51 +0530

    Integrated Refine Query

--- src/controllers/paper.controller.ts
+++ src/controllers/paper.controller.ts
@@ -3,6 +3,7 @@ import { Pinecone } from '@pinecone-database/pinecone';
 import getBertEmbedding from "../utils/bertEmbedding.js";
 import dotenv from 'dotenv';
 import client from "../config/db.js";
+import axios from "axios";
 
 dotenv.config();
 
@@ -58,34 +59,26 @@ export const fetchByQueryAndTag = async (req: Request, res: Response) => {
         const query = req.body.query;
         const difficulty_level = req.body.difficulty_level;
 
-        // Convert the query into a numerical vector that Pinecone can search with
-        const queryEmbedding = await getBertEmbedding(query);
-
-        // Check if embedding is empty
-        if (queryEmbedding.length === 0) {
-            res.status(400).json({
-                message: "Empty embeddings returned",
-                error: "The BERT model returned empty embeddings.",
-            });
-            return;
+        const data = {
+            query: query,
+            difficulty_level: difficulty_level,
+            user_id: "000"
         }
 
-        const queryEmbeddingArray = Array.from(queryEmbedding);
-
-        // Search the index for the three most similar vectors
-        const queryResponse = await index.namespace("ns1").query({
-            topK: 5,
-            vector: queryEmbeddingArray, // The vector is now directly passed
-            includeValues: false,
-            includeMetadata: true
+        const response = await axios.post(`${process.env.AGENT_URL}/query`, data, {
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
         });
 
-        const paperIDs = queryResponse.matches.map(item => item.id);
+        const candidateIDs = response.data; // Extract the candidate IDs from the response
+
 
         const queryText = `SELECT * FROM Papers WHERE arxiv_id = ANY($1) AND difficulty_level = $2;`;
 
         // Execute the query with the ids as an array
-        const result = await client.query(queryText, [paperIDs, difficulty_level]);
+        const result = await client.query(queryText, [candidateIDs, difficulty_level]);
 
         res.json({
             message: "Search completed successfully",
