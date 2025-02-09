a228089c1da864d4a3072ef159b1e7564459d703
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2025-02-09 16:11:50 +0530

    Debugging Vercel Mongo Errors

--- src/controllers/paper.controller.ts
+++ src/controllers/paper.controller.ts
@@ -18,33 +18,29 @@ export const fetchByQuery = async (req: Request, res: Response) => {
         // Define your query
         const query = req.body.query;
 
-        // Convert the query into a numerical vector that Pinecone can search with
-        const queryEmbedding = await getBertEmbedding(query);
+        const data = {
+            query: query,
+            user_id: "000"
+        };
 
-        // Check if embedding is empty
-        if (queryEmbedding.length === 0) {
-            res.status(400).json({
-                message: "Empty embeddings returned",
-                error: "The BERT model returned empty embeddings.",
-            });
-            return;
-        }
-
-        const queryEmbeddingArray = Array.from(queryEmbedding);
-
-        // Search the index for the three most similar vectors
-        const queryResponse = await index.namespace("ns1").query({
-            topK: 5,
-            vector: queryEmbeddingArray, // The vector is now directly passed
-            includeValues: false,
-            includeMetadata: true
+        // Call the external server
+        const response = await axios.post(`${process.env.AGENT_URL}/query`, data, {
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
         });
 
-        console.log(queryResponse);
+        const candidateIDs = response.data; // Extract the candidate IDs from the response
+
+        const queryText = `SELECT * FROM Papers WHERE arxiv_id = ANY($1);`;
+
+        // Execute the query with the IDs as an array
+        const result = await client.query(queryText, [candidateIDs]);
 
         res.json({
             message: "Search completed successfully",
-            results: queryResponse.matches, // Return the matches in the response
+            results: result.rows, // Return the matches in the response
         });
     } catch (error) {
         console.error("Error during query execution:", error);
@@ -54,6 +50,7 @@ export const fetchByQuery = async (req: Request, res: Response) => {
         });
     }
 };
+
 
 export const fetchByQueryAndTag = async (req: Request, res: Response) => {
     try {
